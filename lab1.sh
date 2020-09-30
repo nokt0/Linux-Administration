7d4709870e529c25b42e12b5b67a6a6ff75278bf
vsnimshchikov <vsnimshchikov@emphasoft.com>
2020-09-30 18:00:46 +0300

    fixes

--- lab1.sh
+++ lab1.sh
@@ -2,10 +2,25 @@
 dir_name='test'
 
 function checkDiff() {
-  if [[  -z $(diff -q $1 $2) ]];
+  if [[  -z $(diff -q $1 $2 2>&1) ]];
   then echo "YES"
+  else echo "NO"
   fi
 }
+
+#Deleting
+echo 'print y/n for delete all created files'
+read is_delete
+if [[ "$is_delete" -eq "y" ]]
+then
+rm -rf ~/$dir_name
+rm -rf ~/list1
+rm -rf ~/man.dir
+rm -rf ~/list_conf
+rm -rf ~/list_d
+rm -rf ~/man.txt
+echo 'deleted'
+fi
 
 #Task 1
 mkdir ~/$dir_name
@@ -21,20 +36,45 @@ mkdir ~/$dir_name/links
 ln ~/$dir_name/list ~/$dir_name/links/list_hlink
 #Task 6
 ln -s ~/$dir_name/list ~/$dir_name/links/list_slink
+
+
+#BREAKPOINT
+echo 'links and folders created'
+read
+
+
 #Task 7
 ls -l ~/$dir_name/links | grep 'list_slink' | awk '{print $2}'
 ls -l ~/$dir_name/links | grep 'list_hlink' | awk '{print $2}'
 ls -l ~/$dir_name/ | grep 'list' | awk '{print $2}'
+
+#BREAKPOINT: list_hlink=1 так как указывает только сам на себя на list указывает list и list_hlink
+#            list_slink выдает то же что и list
+echo 'links count'
+read
+
 #Task 8
 wc -l ~/$dir_name/list | awk '{print $1}' >> ~/$dir_name/links/list_hlink
 #Task 9
 checkDiff ~/$dir_name/links/list_hlink ~/$dir_name/links/list_slink
+
+#BREAKPOINT: diff = Yes так как в hlink указывает на link
+#            и при изменении hlink тоже меняется
+echo 'hlink == link'
+read
+
 #Task 10
 mv ~/$dir_name/list ~/$dir_name/list1
 #Task 11
+echo 'list_hlink and list_slink' is equal:
 checkDiff ~/$dir_name/links/list_hlink ~/$dir_name/links/list_slink
+
+#BREAKPOINT: No так как файл не находится и поток ошибок направлен в поток вывода,
+#            условие проверяет строку на пустоту
+read
+
 #Task 12
-ln ~/$dir_name/list1 ~/$dir_name/links/list_link
+ln ~/$dir_name/links/list_hlink ~/list1 
 #Task 13
 find /etc -name "*.conf" > ~/list_conf
 #Task 14
@@ -43,14 +83,24 @@ find /etc -type d -name "*.d" > ~/list_d
 touch ~/list_conf_d
 cat ~/list_conf > ~/list_conf_d
 cat ~/list_d >> ~/list_conf_d
+
+#BREAKPOINT
+echo 'Check created files'
+read
+
 #Task 16
 mkdir ~/$dir_name/.sub
 #Task 17
-mv ~/list_conf_d ~/$dir_name/.sub/
+cp ~/list_conf_d ~/$dir_name/.sub/
 #Task 18
-mv -b ~/list_conf_d ~/$dir_name/.sub/
+cp -b ~/list_conf_d ~/$dir_name/.sub/
 #Task 19
 ls ~/test -A -R
+
+#BREAKPOINT
+echo 'All catalogs'
+read
+
 #Task 20
 man man > ~/man.txt
 #Task 21
@@ -59,14 +109,20 @@ split -b 1k ~/man.txt man_
 #Task 22
 mkdir ~/man.dir
 #Task 23
-find ~ -name "*man_*" -type f -exec mv {} ~/man.dir \;
+find ~ -name "*man_*" 2>/dev/null -type f -exec mv {} ~/man.dir \;
 #Task 24 
 cat ~/man.dir/man_* > ~/man.dir/man.txt
 #Task 25
+echo 'Is files equal?'
 checkDiff ~/man.txt ~/man.dir/man.txt
+
+#BREAKPOINT: yes так как файлы правильно были склеены правильно
+#            и в итоге оказались идентичны
+read
+
 #Task 26
 sed -i '1iBEGIN\nBEGIN\nBEGIN\n' ~/man.txt
-echo 'END\nEND\nEND' >> ~/man.txt
+echo "END\nEND\nEND" >> ~/man.txt
 #Task 27
 diff -u ~/man.txt ~/man.dir/man.txt > ~/man-patch
 #Task 28
@@ -74,4 +130,8 @@ mv ~/man-patch ~/man.dir/
 #Task 29
 patch  ~/man.dir/man.txt < ~/man.dir/man-patch
 #Task 30
+echo 'Is files equal?'
 checkDiff ~/man.txt ~/man.dir/man.txt
+
+#BREAKPOINT: yes так как применился патч
+read
